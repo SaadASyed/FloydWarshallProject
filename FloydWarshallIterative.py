852d7fc09b6b4af57df7dd4e5630bd318fb47671
SaadASyed <saadahmedsyed@hotmail.com>
2023-02-18 00:36:57 +0000

    Adding the iterative version of the algorithm.

---
+++ FloydWarshallIterative.py
@@ -0,0 +1,28 @@
+# This is the iterative version of the Floyd Warshall Algorithm
+# provided as an example for this project. We will be performing
+# performance testing to see the time taken for execution.
+
+import itertools
+import sys
+NO_PATH = sys.maxsize
+graph = [[0, 7, NO_PATH, 8],
+         [NO_PATH, 0, 5, NO_PATH],
+         [NO_PATH, NO_PATH, 0, 2],
+         [NO_PATH, NO_PATH, NO_PATH, 0]]
+MAX_LENGTH = len(graph[0])
+
+
+def floyd(distance):
+    for intermediate, start_node, end_node\
+            in itertools.product(range(MAX_LENGTH), range(MAX_LENGTH), range(MAX_LENGTH)):
+        if start_node == end_node:
+            distance[start_node][end_node] = 0
+            continue
+
+        distance[start_node][end_node] = min(distance[start_node][end_node],
+                                             distance[start_node][intermediate] + distance[intermediate][end_node])
+
+    print(distance)
+
+
+floyd(graph)
