7a073c925dbaba29821ce6ec2a570fbcb4e4c0e1
SaadASyed <saadahmedsyed@hotmail.com>
2023-02-19 14:10:54 +0000

    Further updates to time testing code

--- FloydWarshallTimeTest.py
+++ FloydWarshallTimeTest.py
@@ -7,7 +7,7 @@ import FloydWarshallRecursive
 # to execute the iterative version of the algorithm.
 
 time_StartIterative = time.time()
-subprocess.call(['python', 'FloydWarshallProject\\FloydWarshallIterative.py'])
+subprocess.call(['python', 'FloydWarshallIterative.py'])
 time_EndIterative = time.time()
 print("Iterative Code time taken: ", time_EndIterative - time_StartIterative)
 
@@ -15,6 +15,6 @@ print("Iterative Code time taken: ", time_EndIterative - time_StartIterative)
 # to execute the recursive version of the algorithm.
 
 time_StartRecursive = time.time()
-subprocess.call(['python', 'FloydWarshallProject\\FloydWarshallRecursive.py'])
+subprocess.call(['python', 'FloydWarshallRecursive.py'])
 time_EndRecursive = time.time()
 print("Recursive Code time taken: ", time_EndRecursive - time_StartRecursive)
